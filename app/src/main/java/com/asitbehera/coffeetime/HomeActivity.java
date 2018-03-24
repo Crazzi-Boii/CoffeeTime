84fac3ee571437c4808c8f732d40b99d2edd2982
Crazzi_Boii <cool.aryo@gmail.com>
2018-03-24 22:40:33 +0530

    Coffee Pictures added coffee price TextView added

--- app/src/main/java/com/asitbehera/coffeetime/HomeActivity.java
+++ app/src/main/java/com/asitbehera/coffeetime/HomeActivity.java
@@ -1,15 +1,29 @@
 package com.asitbehera.coffeetime;
 
+/*
+* @@@@@@@@@@@@@@@@@@@@@@@@@@@@
+* @@                        @@
+* @@ Author - Crazzi_Boii   @@
+* @@                        @@
+* @@@@@@@@@@@@@@@@@@@@@@@@@@@@
+*
+* Follow me on github - "github.com/Crazzi-Boii"
+*/
 import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import java.text.NumberFormat;
+import java.util.Locale;
 
 public class HomeActivity extends AppCompatActivity {
     int coffeeIndex = -1, clickStatus ; //to know which coffee type is selected
     String coffeeTypes[] = new String[]{"Cappuccino","Americano","Espresso","Macchiato","Mocha","Latte"};
+    Integer picArray[] = new Integer[]{R.drawable.pic0, R.drawable.pic1, R.drawable.pic2, R.drawable.pic3, R.drawable.pic4, R.drawable.pic5};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +39,9 @@ public class HomeActivity extends AppCompatActivity {
        clickStatus = 0;
         io(clickStatus);
     }
+    // This io method work is to give a flawless use of the coffee selector
+    //it's like if someone click right arrrow a number of time the it will stop at a poin n he need to navigate with the left arrow
+    //but i tried to gave an user a better user experience . . so i came up implementing this method . .
     private void io(int cs){
         if(cs == 1){
             coffeeIndex++;
@@ -38,17 +55,34 @@ public class HomeActivity extends AppCompatActivity {
         if (coffeeIndex == -1){
             coffeeIndex = 5;
         }
+        executeTask();
+    }
+
+    void executeTask(){
         displayCoffee(coffeeIndex);
+        displayCoffeePic(coffeeIndex);
+        displayCoffeePrice(coffeeIndex);
+
     }
 
     void displayCoffee(int CI ){ // CI = coffeeIndex
         TextView coffeeTypeTV = (TextView) findViewById(R.id.CoffeeTypeTextView);
-        if (CI >= 0 && CI <= 5);
+        if (CI >= 0 && CI <= 5)
         coffeeTypeTV.setText(coffeeTypes[CI]);
+
        /* TextView testTV = (TextView) findViewById(R.id.testtv);
         testTV.setText(String.valueOf(CI)); */
     }
-
+    void displayCoffeePic(int CI){
+        ImageView coffeePictureIV = (ImageView) findViewById(R.id.coffeePicture);
+        coffeePictureIV.setImageResource(picArray[CI]);
+    }
+    void displayCoffeePrice(int CI){
+        TextView coffeePriceTV = (TextView) findViewById(R.id.coffeePriceTextView);
+        coffeePriceTV.setVisibility(View.VISIBLE);
+        String total = NumberFormat.getCurrencyInstance(new Locale("en", "in")).format(CI);
+        coffeePriceTV.setText(total);
+    }
 
     public void tost(View view){
         Toast toast;
