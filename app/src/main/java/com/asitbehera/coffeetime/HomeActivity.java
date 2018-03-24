8497195f3d86722438490836e5d4c79170b9cf3e
Crazzi_Boii <cool.aryo@gmail.com>
2018-03-25 02:37:30 +0530

    fully functional home activity

--- app/src/main/java/com/asitbehera/coffeetime/HomeActivity.java
+++ app/src/main/java/com/asitbehera/coffeetime/HomeActivity.java
@@ -10,6 +10,7 @@ package com.asitbehera.coffeetime;
 * Follow me on github - "github.com/Crazzi-Boii"
 */
 import android.content.Context;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -21,7 +22,8 @@ import java.text.NumberFormat;
 import java.util.Locale;
 
 public class HomeActivity extends AppCompatActivity {
-    int coffeeIndex = -1, clickStatus ; //to know which coffee type is selected
+    int coffeeIndex = -1, clickStatus, quantity = 0; //to know which coffee type is selected
+    Toast toast;
     String coffeeTypes[] = new String[]{"Cappuccino","Americano","Espresso","Macchiato","Mocha","Latte"};
     Integer picArray[] = new Integer[]{R.drawable.pic0, R.drawable.pic1, R.drawable.pic2, R.drawable.pic3, R.drawable.pic4, R.drawable.pic5};
 
@@ -30,14 +32,34 @@ public class HomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
+
     }
     public void  incr(View view){
         clickStatus = 1;
         io(clickStatus);
     }
     public void  decr(View view){
-       clickStatus = 0;
+        clickStatus = 0;
         io(clickStatus);
+    }
+    public void  qincr(View view){
+        if (quantity < 10){
+            quantity++;
+            displayQuantity();
+        }
+        if(quantity >= 10){
+            makeToast("Max 10 cups!");
+        }
+    }
+    public void  qdecr(View view){
+        if (quantity > 0){
+            quantity--;
+            displayQuantity();
+        }
+    }
+    void displayQuantity(){
+        TextView displayQuantityTV = (TextView) findViewById(R.id.showQuantity);
+        displayQuantityTV.setText("" + quantity);
     }
     // This io method work is to give a flawless use of the coffee selector
     //it's like if someone click right arrrow a number of time the it will stop at a poin n he need to navigate with the left arrow
@@ -83,11 +105,103 @@ public class HomeActivity extends AppCompatActivity {
         String total = NumberFormat.getCurrencyInstance(new Locale("en", "in")).format(CI);
         coffeePriceTV.setText(total);
     }
+    int XtrachinaStatus = 0;
+    public void xtraChina(View view) {
+        TextView xtraChinaTV = (TextView) findViewById(R.id.xtraChinaTextView);
+        if (XtrachinaStatus == 0){
+            xtraChinaTV.setTextColor(Color.RED);
+            makeToast("Extra chinnamon added.");
+            XtrachinaStatus=1;
+         }else if (XtrachinaStatus == 1){
+            xtraChinaTV.setTextColor(Color.parseColor("#211f21"));
+            makeToast("Extra chinnamon removed.");
+            XtrachinaStatus=0;
+        }
+    }
+    int XtrachocoStatus = 0;
+    public void XtraChoco(View view){
+        TextView xtraChocoTV = (TextView) findViewById(R.id.xtraChoco);
+        if (XtrachocoStatus == 0){
+            xtraChocoTV.setTextColor(Color.RED);
+            makeToast("Extra chocolate syrup added.");
+            XtrachocoStatus=1;
+        }else if (XtrachocoStatus == 1){
+            xtraChocoTV.setTextColor(Color.parseColor("#211f21"));
+            makeToast("Extra chocolate syrup removed.");
+            XtrachocoStatus=0;
+        }
+    }
+    /***************************************************
+     * *************************************************
+     *
+     * Size Methods Starts
+     *
+     * ***************************************************
+     * ***************************************************/
+    int coffeeSize ;
+    public void sizeS(View view){
+        coffeeSize = 1;
+        sizeExecute();
+    }
+    public void sizeM(View view){
+        coffeeSize = 2;
+        sizeExecute();
+    }
+    public void sizeL(View view){
+        coffeeSize = 3;
+        sizeExecute();
+    }
+    public void sizeXL(View view){
+        coffeeSize = 4;
+        sizeExecute();
+    }
 
-    public void tost(View view){
-        Toast toast;
+    void sizeExecute(){
+        TextView smallTV = (TextView) findViewById(R.id.small);
+        TextView mediumTV = (TextView) findViewById(R.id.medium);
+        TextView largeTV = (TextView) findViewById(R.id.large);
+        TextView xlargeTV = (TextView) findViewById(R.id.Xlarge);
+        sizeReset();
+        switch (coffeeSize){
+            case 1:{
+                smallTV.setTextColor(Color.RED);
+                break;
+            }
+            case 2:{
+                mediumTV.setTextColor(Color.RED);
+                break;
+            }
+            case 3:{
+                largeTV.setTextColor(Color.RED);
+                break;
+            }
+            case 4:{
+                xlargeTV.setTextColor(Color.RED);
+                break;
+            }
+        }
+    }
+    void sizeReset(){
+        TextView smallTV = (TextView) findViewById(R.id.small);
+        TextView mediumTV = (TextView) findViewById(R.id.medium);
+        TextView largeTV = (TextView) findViewById(R.id.large);
+        TextView xlargeTV = (TextView) findViewById(R.id.Xlarge);
+        smallTV.setTextColor(Color.parseColor("#211f21"));
+        mediumTV.setTextColor(Color.parseColor("#211f21"));
+        largeTV.setTextColor(Color.parseColor("#211f21"));
+        xlargeTV.setTextColor(Color.parseColor("#211f21"));
+    }
+    /***************************************************
+     * *************************************************
+     *
+     * Size Methods Emds
+     *
+     * ***************************************************
+     * ****************************************************/
+
+
+    public void makeToast(CharSequence text){
         Context context = getApplicationContext();
-        CharSequence text = "Hello toast!";
         int duration = Toast.LENGTH_SHORT;
         toast = Toast.makeText(context, text, duration);
         toast.show();
